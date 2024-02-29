9190df41c2bc9fb657c1640b82b367e6558fc4d9
Julia Vorotchenko <juliavorotchenko@gmail.com>
2024-02-29 17:40:31 +0200

    Add decorators to initial bot from first task

---
+++ Bot/assistant_bot.py
@@ -0,0 +1,37 @@
+class AssistantBot:
+    def __init__(self):
+        self.contacts = {}
+
+    def parse_input(self, user_input):
+        cmd, *args = user_input.split()
+        cmd = cmd.strip().lower()
+        return cmd, args
+
+    def add_contact(self, args):
+        name, phone = args
+        self.contacts[name] = phone
+        return "Contact added."
+
+    def change_contact_phone(self, args):
+        name, phone = args
+        if name in self.contacts:
+            self.contacts[name] = phone
+            return "Phone number updated."
+        else:
+            return "Contact not found."
+
+    def display_contact_phone(self, args):
+        name = args[0]
+        if name in self.contacts:
+            return f"{name}'s phone number: {self.contacts[name]}"
+        else:
+            return "Contact not found."
+
+    def display_all_contacts(self):
+        if self.contacts:
+            result = "All contacts:\n"
+            for name, phone in self.contacts.items():
+                result += f"{name}: {phone}\n"
+            return result.strip()
+        else:
+            return "No contacts available."
